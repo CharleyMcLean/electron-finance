a94932005e4f4dc34d73797095c6de2b38664e59
CharleyMcLean <charleyaustinmclean@gmail.com>
2016-12-16 13:50:26 -0800

    Changed colors, added percent increase or decrease, added a couple of extra funds.

--- window.js
+++ window.js
@@ -2,52 +2,89 @@
 $(function () {
 
 var stocks = [
-  "CL=F", // Crude oil, http://finance.yahoo.com/quote/CL=F?p=CL=F
-  "GC=F", // Gold, https://finance.yahoo.com/quote/GC=F?p=GC=F
-  "SI=F"  // Silver, https://finance.yahoo.com/quote/SI=F?p=SI=F
-]
+  "SMLPX", // SMLPX, https://finance.yahoo.com/quote/SMLPX?p=SMLPX
+  "SRPFX", // SRPFX, https://finance.yahoo.com/quote/SRPFX?p=SRPFX
+  "KIFAX",  // KIFAX, https://finance.yahoo.com/quote/KIFAX/?p=KIFAX
+  "AHBAX", // AHBAX, https://finance.yahoo.com/quote/AHBAX/?p=AHBAX
+  "KGIYX", // KGIYX, https://finance.yahoo.com/quote/KGIYX/?p=KGIYX
+];
 
-var fields = 'f=pl1' // Requests the current price and previous closing price
-var symbols = 's=' + stocks.join('+')
-var url = 'https://finance.yahoo.com/d/quotes.csv?' + fields + '&' + symbols
+var fields = 'f=pl1'; // Requests the current price and previous closing price
+var symbols = 's=' + stocks.join('+');
+var url = 'https://finance.yahoo.com/d/quotes.csv?' + fields + '&' + symbols;
 
 $.ajax(url).done(function (csv) {
   // Split the output up into an array of lines
-  var lines = csv.trim().split('\n')
+  var lines = csv.trim().split('\n');
 
   // Iterate over each line
   for (var i = 0; i < lines.length; i++) {
     //Split the line up by comma
-    var prices = lines[i].split(',')
+    var prices = lines[i].split(',');
 
     // Previous closing price of stock symbol
-    var previousPrice = parseFloat(prices[0], 10)
+    var previousPrice = parseFloat(prices[0], 10);
 
     // Current price of stock symbol
-    var currentPrice = parseFloat(prices[1], 10)
+    var currentPrice = parseFloat(prices[1], 10);
 
     // Change between closing price and current price rounded to 2 decimal points.
-    var change = Math.round((currentPrice - previousPrice) * 100) / 100
+    var change = Math.round((currentPrice - previousPrice) * 100) / 100;
+
+    // Percent change between closing price and current price rounded to 2 decimal points.
+    var percentChange = Math.round((currentPrice - previousPrice) / previousPrice * 100 * 100) / 100;
 
     // Add a leading + for positive change
     if (change >= 0) {
-      change = '+' + change
+        change = '+' + change;
     }
 
+    // Add a leading + for positive percent change
+    if (percentChange >= 0) {
+        percentChange = '+' + percentChange;
+    }
+
+
     // Add prices and changes to HTML element
-    if (i == 0) { // Oil
-      $('#oil-price').text(currentPrice.toLocaleString())
-      $('#oil-change').text(change)
-    } else if (i == 1) { // Gold
-      $('#gold-price').text(currentPrice.toLocaleString())
-      $('#gold-change').text(change)
-    } else if (i == 2) { // Silver
-      $('#silver-price').text(currentPrice.toLocaleString())
-      $('#silver-change').text(change)
+    if (i == 0) { // SMLPX
+        $('#smlpx-price').text(currentPrice.toLocaleString());
+        $('#smlpx-change').text(change);
+        $('#smlpx-percentChange').text(percentChange);
+    } else if (i == 1) { // SRPFX
+        $('#srpfx-price').text(currentPrice.toLocaleString());
+        $('#srpfx-change').text(change);
+        $('#srpfx-percentChange').text(percentChange);
+    } else if (i == 2) { // KIFAX
+        $('#kifax-price').text(currentPrice.toLocaleString());
+        $('#kifax-change').text(change);
+        $('#kifax-percentChange').text(percentChange);
+    } else if (i == 3) { // AHBAX
+        $('#ahbax-price').text(currentPrice.toLocaleString());
+        $('#ahbax-change').text(change);
+        $('#ahbax-percentChange').text(percentChange);
+    } else if (i == 4) { // KGIYX
+        $('#kgiyx-price').text(currentPrice.toLocaleString());
+        $('#kgiyx-change').text(change);
+        $('#kgiyx-percentChange').text(percentChange);
     }
   }
 }).fail(function (error) {
-  console.error(error)
-})
+    console.error(error);
+});
 
-})
+var shell = require('electron').shell;
+//open links externally by default
+// $("#smplx").on('click', 'a[href^="https://finance.yahoo.com/quote/SMLPX?p=SMLPX"]', function(event) {
+//     event.preventDefault();
+//     shell.openExternal(this.href);
+// });
+
+function myFunction() {
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
+
+    var win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadURL('www.google.com');
+}
+
+});
